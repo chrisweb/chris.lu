8a91a7c1b1dcc001db0b5944465b84699e566578
chrisweb <chris963@gmail.com>
2023-02-07 21:44:59 +0100

    converted config to mjs

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import WithBundleAnalyzer from '@next/bundle-analyzer'
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
+
+const nextConfig = (phase) => {
+
+    const withBundleAnalyzer = WithBundleAnalyzer({
+        enabled: phase === PHASE_DEVELOPMENT_SERVER ? true : false,
+        //openAnalyzer: false,
+    })
+
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+        experimental: {
+            appDir: true,
+        },
+        images: {
+            formats: ['image/avif', 'image/webp']
+        },
+    }
+
+    return withBundleAnalyzer(nextConfig)
+
+}
+
+export default nextConfig
