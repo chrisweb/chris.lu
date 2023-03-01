1501333ab3a1a5e64a345e3ea12a6ec42a3a51f1
chrisweb <chris963@gmail.com>
2023-03-01 20:09:44 +0100

    remote mdx experiments

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,10 +1,9 @@
 import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import { dirname/*, join*/ } from 'path'
 import { globby } from 'globby'
 
-
-import fs from 'fs'
-import { MDXRemote, compileMDX } from 'next-mdx-remote/rsc'
+//import fs from 'fs'
+//import { MDXRemote, compileMDX } from 'next-mdx-remote/rsc'
 
 interface IPageProps {
     params: {
@@ -16,11 +15,18 @@ export const dynamicParams = false
 
 export async function generateStaticParams() {
 
-    const directoryPath = dirname(fileURLToPath(import.meta.url))
+    const contentDirectory = dirname(fileURLToPath(import.meta.url))
 
-    const contentDirectory = join(directoryPath, '..', '(content)').replace(/\\/g, '/')
+    console.log(contentDirectory)
+
+    //const contentDirectory = join(directoryPath, '..', '(content)').replace(/\\/g, '/')
+
+    // convert to posix format as required by globby
+    //const contentDirectory = directoryPath.replace(/\\/g, '/')
 
     const files = await globby('*.mdx', { cwd: contentDirectory })
+
+    console.log(files)
 
     return files.map((file) => {
         const slug = file.replace('.mdx', '')
@@ -33,11 +39,15 @@ export async function generateStaticParams() {
 
 export default async function Article(props: IPageProps) {
 
-    console.log(props)
-
     const { params: { slug } } = props
 
-    const fileName = slug + '.mdx'
+    return (
+        <>
+            <span>{slug}</span>
+        </>
+    )
+
+    /*const fileName = slug + '.mdx'
 
     const directoryPath = dirname(fileURLToPath(import.meta.url))
 
@@ -56,19 +66,19 @@ export default async function Article(props: IPageProps) {
             </h1>
         ),
     }
-
+*/
     // there is a bug in the types for MDXRemoteProps
     // source replaces compiledSource from MDXRemoteSerializeResult
     // https://github.com/hashicorp/next-mdx-remote/issues/336
-    // @ts-expect-error will be fixed in next release of next-mdx-remote/rsc
-    const { content, frontmatter } = await compileMDX({ source: contentMDX, options: mdxOptions, mdxComponents })
+    // // @ts-expect-error will be fixed in next release of next-mdx-remote/rsc
+    //const { content, frontmatter } = await compileMDX({ source: contentMDX, options: mdxOptions, mdxComponents })
 
-    console.log('frontmatter: ', frontmatter)
+    //console.log('frontmatter: ', frontmatter)
 
-    return (
+    /*return (
         <>
-            {/*<MDXRemote source={contentMDX} components={mdxComponents}  />*/}
+            <MDXRemote source={contentMDX} components={mdxComponents}  />
             {content}
         </>
-    )
+    )*/
 }
