572293e515e1dd368943e618c3544527898b3701
chrisweb <chris963@gmail.com>
2023-03-11 04:40:44 +0100

    add play button to launch animation

--- components/neonRoad/Container.tsx
+++ components/neonRoad/Container.tsx
@@ -1,19 +1,46 @@
 'use client'
 
+import { useState } from 'react'
+import StaticImage from './StaticImage'
+import PlayButton from './PlayButton'
 import dynamic from 'next/dynamic'
 // in this dynamic import case it is important to set "ssr: false"
 // as in the NeonRoadCanvas component we use window
 // else you get "window is not defined"
 const NeonRoadCanvas = dynamic(() => import('./Canvas'), {
     ssr: false,
-    loading: () => <span style={{ color: 'white', fontSize: '30px' }}>Loading...</span>,
+    loading: () => <div style={{
+        color: 'white',
+        fontSize: '30px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
+    }}>Loading...</div>,
 })
 
 const Container: React.FC = () => {
 
+    const [showAnimationState, setShowAnimationState] = useState(false)
+
+    const clickPlayCallback = () => {
+        setShowAnimationState(true)
+    }
+
+    const altText = 'Chris.lu header image, displaying an 80s style landscape and sunset'
+
     return (
         <>
-            <NeonRoadCanvas />
+            {!showAnimationState &&
+                <>
+                    <StaticImage altText={altText} />
+                    <PlayButton clickPlayCallback={clickPlayCallback} />
+                </>
+            }
+            {showAnimationState &&
+                <NeonRoadCanvas altText={altText} />
+            }
         </>
     )
 }
