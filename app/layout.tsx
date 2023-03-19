d861ff4826da31812ce0df6d861f381f378e2a85
chrisweb <chris963@gmail.com>
2023-03-19 19:52:34 +0100

    content structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,13 @@ export default function RootLayout({ children }: {
                     <div className={styles.layoutNavbarContainer}>
                         <nav className={styles.layoutNavbar}>
                             <Link href="/">Home</Link>
-                            <Link href="/dev">Web development</Link>
-                            <Link href="/lego">Lego bricks</Link>
+                            <Link href="/web_development">Web development</Link>
+                            <Link href="/lego">Lego</Link>
                             <Link href="/music">Music</Link>
+                            <Link href="/games">Games</Link>
+                            <Link href="/cooking">Cooking</Link>
+                            <Link href="/movies_and_tv_shows">Movies & TV shows</Link>
+                            <Link href="/memes">Memes</Link>
                         </nav>
                     </div>
                     <h1 className={styles.eighties}>Chris.lu</h1>
---
+++ app/web_development/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'foo',
+    description: 'bar',
+}
+
+export default function WebDevelopmentPage() {
+
+    return (
+        <>
+
+        </>
+    )
+}
