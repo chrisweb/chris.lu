d66f4fe2377b2916a2b8be7e2adbe2bbc2d864de
chrisweb <chris963@gmail.com>
2023-06-29 23:44:39 +0200

    custom MDX components, headings observer (wip)

---
+++ hooks/useObserver.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState, useRef } from 'react'
+
+export function useObserver(elementsToObserve: string, rootMargin: string) {
+
+    const [activeIdState, setActiveIdState] = useState('')
+    const observerRef = useRef<IntersectionObserver>()
+
+    useEffect(() => {
+
+        const handleObsever = (entries) => {
+            entries.forEach((entry) => {
+                if (entry?.isIntersecting) {
+                    setActiveIdState(entry.target.id)
+                }
+            })
+        }
+
+        if (observerRef !== undefined) {
+            observerRef.current = new IntersectionObserver(handleObsever, {
+                rootMargin: rootMargin
+            })
+
+            const elements = document.querySelectorAll(elementsToObserve)
+
+            elements.forEach((elem) => observerRef.current.observe(elem))
+        }
+
+        return () => {
+            observerRef.current?.disconnect()
+        }
+    }, [elementsToObserve])
+
+    return { activeIdState }
+
+}
