7be384d4fab9d3e8561b9d9a756db5c93d21abde
chrisweb <chris963@gmail.com>
2023-10-25 22:37:23 +0200

    fix link

--- app/web_development/page.tsx
+++ app/web_development/page.tsx
@@ -9,11 +9,11 @@ export const metadata: Metadata = {
 export default function WebDevelopmentPage() {
 
     return (
-        <>
+        <section>
             <br /><br />
             <Link href="/web_development/test" className="a">04.06.2023 first article</Link>
             <br /><br />
-            <Link href="/web_development/next-js-static-mdx-blog" className="a">ESLint</Link>
-        </>
+            <Link href="/web_development/next-js-static-mdx-blog/eslint" className="a">ESLint</Link>
+        </section>
     )
 }
