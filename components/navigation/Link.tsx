ee7136581e25a936382d503a59060394e0b3a1d7
chrisweb <chris963@gmail.com>
2023-10-26 00:02:13 +0200

    add icon to external links

--- components/navigation/Link.tsx
+++ components/navigation/Link.tsx
@@ -1,20 +1,51 @@
-import Link from 'next/link'
 import { ReactNode } from 'react'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 export interface INavigationLinkProps {
     href: string
     children: ReactNode
 }
 
+const isExternalUrl = (url: string, domain: string): boolean => {
+
+    const urlLowerCase = url.toLowerCase()
+    
+    const firstCharacter = urlLowerCase.charAt(0)
+
+    if (firstCharacter === '#' || firstCharacter === '/') {
+        return false
+    }
+
+    if (urlLowerCase.slice(0,7) === 'http://' || urlLowerCase.slice(0,8) === 'https://') {
+
+        const urlNoProtocol = urlLowerCase.replace('http://','').replace('https://','')
+
+        const potentialDomain = urlNoProtocol.split('/')[0]
+
+        if (potentialDomain !== domain) {
+            return true
+        }
+
+    }
+
+    return false
+
+}
+
 const NavigationLink: React.FC<INavigationLinkProps> = (props): JSX.Element => {
 
     const { href, children, ...rest } = props
+
+    const isExternal = isExternalUrl(href, 'chris.lu')
 
     return (
         <>
             <Link href={href} {...rest}>
                 {children}
             </Link>
+            {isExternal && <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="sm" className='externalLinkIcon' />}
         </>
     )
 }
