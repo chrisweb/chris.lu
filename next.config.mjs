71f514796e16be98c87cbd9a44b213c2a9309843
chrisweb <chris963@gmail.com>
2023-11-12 01:05:16 +0100

    styling alerts

--- next.config.mjs
+++ next.config.mjs
@@ -132,28 +132,81 @@ const nextConfig = (/*phase*/) => {
 
     // https://github.com/chrisweb/rehype-github-alerts
     /** @type { DefaultBuildType } */
-    /*const myGithubAlertBuild = (alertOptions, originalChildren) => {
+    const myGithubAlertBuild = (alertOptions, originalChildren) => {
+
         const alert = {
             type: 'element',
             tagName: 'div',
             properties: {
                 className: [
+                    'markdown-alert',
                     `markdown-alert-${alertOptions.keyword.toLowerCase()}`,
                 ],
-                style: 'color: ' + alertOptions.color + ';'
             },
             children: [
-                ...originalChildren
-            ]
+                {
+                    type: 'element',
+                    tagName: 'div',
+                    properties: {
+                        className: [
+                            'makrdown-alert-fake-border',
+                        ],
+                    },
+                    children: [
+                        {
+                            type: 'element',
+                            tagName: 'span',
+                            properties: {
+                                className: [
+                                    'markdown-alert-header'
+                                ],
+                            },
+                            children: [
+                                {
+                                    type: 'text',
+                                    value: alertOptions.title
+                                }
+                            ]
+                        },
+                        {
+                            type: 'element',
+                            tagName: 'p',
+                            properties: {
+                                className: [
+                                    'markdown-alert-body'
+                                ],
+                            },
+                            children: [
+                                ...originalChildren
+                            ]
+                        },
+                    ],
+                },
+            ],
         }
 
         return alert
-    }*/
+
+    }
 
     /** @type { IOptions } */
     const rehypeGithubAlertsOptions = {
         supportLegacy: false,
-        //build: myGithubAlertBuild
+        build: myGithubAlertBuild,
+        alerts: [
+            {
+                keyword: 'NOTE',
+                icon: '<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.568 1.031A6.8 6.8 0 0 1 12.76 3.05a7.06 7.06 0 0 1 .46 9.39 6.85 6.85 0 0 1-8.58 1.74 7 7 0 0 1-3.12-3.5 7.12 7.12 0 0 1-.23-4.71 7 7 0 0 1 2.77-3.79 6.8 6.8 0 0 1 4.508-1.149zM9.04 13.88a5.89 5.89 0 0 0 3.41-2.07 6.07 6.07 0 0 0-.4-8.06 5.82 5.82 0 0 0-7.43-.74 6.06 6.06 0 0 0 .5 10.29 5.81 5.81 0 0 0 3.92.58zM7.375 6h1.25V5h-1.25v1zm1.25 1v4h-1.25V7h1.25z"/></svg>',
+                color: 'rgb(9, 105, 218)',
+                title: 'Note',
+            },
+            {
+                keyword: 'WARNING',
+                icon: '<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.568 1.031A6.8 6.8 0 0 1 12.76 3.05a7.06 7.06 0 0 1 .46 9.39 6.85 6.85 0 0 1-8.58 1.74 7 7 0 0 1-3.12-3.5 7.12 7.12 0 0 1-.23-4.71 7 7 0 0 1 2.77-3.79 6.8 6.8 0 0 1 4.508-1.149zM9.04 13.88a5.89 5.89 0 0 0 3.41-2.07 6.07 6.07 0 0 0-.4-8.06 5.82 5.82 0 0 0-7.43-.74 6.06 6.06 0 0 0 .5 10.29 5.81 5.81 0 0 0 3.92.58zM7.375 6h1.25V5h-1.25v1zm1.25 1v4h-1.25V7h1.25z"/></svg>',
+                color: 'rgb(9, 105, 218)',
+                title: 'Warning',
+            },
+        ],
     }
 
     const withMDX = createMdx({
