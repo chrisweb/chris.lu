9c93b1d6db84aafd4770be05d9067bc6a7885569
chrisweb <chris963@gmail.com>
2023-12-29 00:35:27 +0100

    fix displacement map name

--- components/neonRoad/Terrain.tsx
+++ components/neonRoad/Terrain.tsx
@@ -4,6 +4,7 @@ import { forwardRef } from 'react'
 import type { Mesh } from 'three'
 import type { MeshProps } from '@react-three/fiber'
 import { useTexture } from '@react-three/drei'
+//import { NearestFilter , NearestMipmapNearestFilter } from 'three'
 
 export interface IProps extends MeshProps {
     zPosition: number
@@ -12,7 +13,7 @@ export interface IProps extends MeshProps {
 const Terrain: React.FC<IProps> = forwardRef<Mesh, IProps>((props: IProps, terrainRef: React.MutableRefObject<Mesh>) => {
 
     const FLOOR_TEXTURE_PATH = '/assets/images/neonroad/grid_4096x8192-min.png'
-    const DISPLACEMENT_MAP_PATH = '/assets/images/neonroad/displacement_32x64-min.png'
+    const DISPLACEMENT_MAP_PATH = '/assets/images/neonroad/displacement_map_32x64-min.png'
     //const DISPLACEMENT_MAP_PATH = '/assets/images/neonroad/displacement_map_128x256.png'
     const EMISSIVE_MAP_PATH = '/assets/images/neonroad/emissive_map_4096x8192-min.png'
 
@@ -29,9 +30,11 @@ const Terrain: React.FC<IProps> = forwardRef<Mesh, IProps>((props: IProps, terra
     floorTexture.anisotropy = 2
 
     // https://threejs.org/examples/#webgl_materials_texture_filters
-    //import { LinearFilter, LinearMipmapLinearFilter } from 'three'
-    //floorTexture.magFilter = LinearFilter
-    //floorTexture.minFilter = LinearMipmapLinearFilter
+    // https://threejs.org/docs/#api/en/textures/Texture.magFilter
+    // https://threejs.org/docs/index.html#api/en/constants/Textures
+    //import { NearestFilter , NearestMipmapNearestFilter } from 'three'
+    //floorTexture.magFilter = NearestFilter
+    //floorTexture.minFilter = NearestMipmapNearestFilter
 
     const displacementScale = 0.5
 
