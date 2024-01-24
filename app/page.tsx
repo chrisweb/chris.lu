654a6509c08d580da4e1c9788d23551a38c5ab23
chrisweb <chris963@gmail.com>
2024-01-24 20:26:09 +0100

    update homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -20,20 +20,29 @@ export default function Homepage() {
                             </div>
                         </div>
                     </div>
-                    <div className={styles.cyberPortalOuterGlow} id="lego">
-                        <div className={styles.cyberPortalBorder}>
-                            <div className={styles.cyberPortalInnerGlow}>
-                                <div className={styles.cyberPortalCore}>
-                                    <p><Link href="/about_me" className="hyperLink">About me:</Link> I don&apos;t think most of the content of this blog will be about me, as I intend to mostly write about my hobbies, but I also wanted to have a more personal section where I share a brief (and obviously biased) description of myself. Over time I will eventually post some more personal articles not linked to any hobby.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div className={styles.cyberPortalOuterGlow} id="games">
                         <div className={styles.cyberPortalBorder}>
                             <div className={styles.cyberPortalInnerGlow}>
                                 <div className={styles.cyberPortalCore}>
-                                    <p>Coming soon...</p>
+                                    <p><Link href="/games" className="hyperLink">Games:</Link> I have always liked playing video games, be it on consoles, PC and yes even mobile (I know shame on me 😉). Playing games is my oldest hobby I still enjoy these days. I like playing games and also like watching people play on Twitch and sometimes I even do both at the same time.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cyberPortalOuterGlow} id="lego">
+                        <div className={styles.cyberPortalBorder}>
+                            <div className={styles.cyberPortalInnerGlow}>
+                                <div className={styles.cyberPortalCore}>
+                                    <p><Link href="/lego" className="hyperLink">Lego:</Link> If there is one hobby that helps me chill after a busy day it for sure is building things using bricks. I like building a lot as I can do it in the evening while also bing watching my favorite TV series or just listening to music. I like being creative and like watching Videos or Streams from other AFOLs.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.cyberPortalOuterGlow} id="about_me">
+                        <div className={styles.cyberPortalBorder}>
+                            <div className={styles.cyberPortalInnerGlow}>
+                                <div className={styles.cyberPortalCore}>
+                                    <p><Link href="/about_me" className="hyperLink">About me:</Link> Everything on this blog is already about me, but I also wanted to have a more personal area where I can write about things that are not related to web development, playing games and building with bricks.</p>
                                 </div>
                             </div>
                         </div>
