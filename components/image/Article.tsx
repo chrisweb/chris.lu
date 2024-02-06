ea48a2de1d0f1c3717052ae6cc491b38ff6f2801
chrisweb <chris963@gmail.com>
2024-02-06 01:58:09 +0100

    if gif dont use placeholder

--- components/image/Article.tsx
+++ components/image/Article.tsx
@@ -2,6 +2,15 @@ import Image, { ImageProps } from 'next/image'
 
 const ImageArticle: React.FC<ImageProps> = (props): JSX.Element => {
 
+    let placeholder = true
+
+    // something is weird because
+    // src is of type StaticImport, but somehow ts thinks it has no properties
+    // @ts-expect-error: because the library definition is wrong
+    if (props.src?.src.slice(-3) === 'gif') {
+        placeholder = false
+    }
+
     return (
         <>
             {props.alt.startsWith('banner') ? (
@@ -12,7 +21,7 @@ const ImageArticle: React.FC<ImageProps> = (props): JSX.Element => {
                     }}
                     sizes="100vw"
                     priority
-                    placeholder="blur"
+                    placeholder={ placeholder ? 'blur' : 'empty' }
                     //quality={90} // default is 75
                     {...(props as ImageProps)}
                 />
@@ -23,7 +32,7 @@ const ImageArticle: React.FC<ImageProps> = (props): JSX.Element => {
                         height: 'auto',
                     }}
                     sizes="100vw"
-                    placeholder="blur"
+                    placeholder={ placeholder ? 'blur' : 'empty' }
                     {...(props as ImageProps)}
                 />
             ) : (
@@ -32,7 +41,7 @@ const ImageArticle: React.FC<ImageProps> = (props): JSX.Element => {
                         maxWidth: '100%',
                         height: 'auto',
                     }}
-                    placeholder="blur"
+                    placeholder={ placeholder ? 'blur' : 'empty' }
                     {...(props as ImageProps)}
                 />
             )}
