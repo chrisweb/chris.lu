2b53e4d335ae16a61d0ab4e2cff6f51dbe471e99
chrisweb <chris963@gmail.com>
2024-03-01 23:15:26 +0100

    add link to aws nodejs tutorial and csp post

--- app/web_development/page.tsx
+++ app/web_development/page.tsx
@@ -37,12 +37,20 @@ export default function WebDevelopmentPage() {
                         <Image src="/assets/images/app/web_development/tutorials/xcode-cloud-capacitor-webapp/banner.png" alt="" fill sizes="400px" style={{ objectFit: 'cover' }} />
                         <div className={styles.title}>Xcode Cloud CI/CD to distribute a capacitor app to TestFlight users and the App Store</div>
                     </Link>
+                    <Link href="/web_development/tutorials/node-js-app-aws-ec2" className={styles.preview}>
+                        <Image src="/assets/images/app/web_development/tutorials/node-js-app-aws-ec2/banner.png" alt="" fill sizes="400px" style={{ objectFit: 'cover' }} />
+                        <div className={styles.title}>Node.js app deployment on AWS EC2 instance with NGINX reverse proxy</div>
+                    </Link>
                 </div>
                 <h2 className="h2">Posts</h2>
                 <div className="grid">
                     <Link href="/web_development/posts/road-to-react-next-js-server-side-features" className={styles.preview}>
                         <Image src="/assets/images/app/web_development/posts/road-to-react-next-js-server-side-features/banner.png" alt="" fill sizes="400px" style={{ objectFit: 'cover' }} />
                         <div className={styles.title}>The road to server components and server actions</div>
+                    </Link>
+                    <Link href="/web_development/posts/csp" className={styles.preview}>
+                        <Image src="/assets/images/app/web_development/posts/csp/banner.png" alt="" fill sizes="400px" style={{ objectFit: 'cover' }} />
+                        <div className={styles.title}>Content Security Policy (CSP)</div>
                     </Link>
                     <Link href="/web_development/posts/vscode" className={styles.preview}>
                         <Image src="/assets/images/app/web_development/posts/vscode/banner.png" alt="" fill sizes="400px" style={{ objectFit: 'cover' }} />
