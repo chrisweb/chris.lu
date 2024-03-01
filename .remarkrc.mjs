340cfd99279662acba9884ef5e5185c5d06507f3
chrisweb <chris963@gmail.com>
2024-03-01 21:56:30 +0100

    disable markdown no html rule

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -10,12 +10,17 @@ const config = {
         remarkPresetLintRecommended,
         remarkPresetLintConsistent,
         remarkPresetLintMarkdownStyleGuide,
+        // full list of rules packages
+        // https://github.com/remarkjs/remark-lint/tree/main/packages
+        //
         // https://www.npmjs.com/package/remark-lint-maximum-line-length
         ['remark-lint-maximum-line-length', false],
         // https://www.npmjs.com/package/remark-lint-list-item-spacing
         ['remark-lint-list-item-spacing', false],
         // https://www.npmjs.com/package/remark-lint-maximum-heading-length
-        ['remark-lint-maximum-heading-length', [1, 48]],
+        ['remark-lint-maximum-heading-length', [1, 50]],
+        // https://www.npmjs.com/package/remark-lint-no-html
+        ['remark-lint-no-html', true],
 
         //remarkGfm,
         //remarkGithub,
