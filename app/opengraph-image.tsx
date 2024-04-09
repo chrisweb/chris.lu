23067a1974ab61315aedfc4c7d40e5b0583e392e
chrisweb <chris963@gmail.com>
2024-04-09 22:30:26 +0200

    add opengraph image generator recreating main banner

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -47,6 +47,7 @@ export default async function OGImage(/*props: IImageProps*/) {
                 }}
             >
                 {
+                    // eslint-disable-next-line @next/next/no-img-element
                     <img
                         // @ts-ignore: this is fine 🔥
                         src={imageData}
