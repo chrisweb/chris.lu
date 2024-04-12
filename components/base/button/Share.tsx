9a71d0e1c04c56a84bd9667b1f116bcdf6726e0a
chrisweb <chris963@gmail.com>
2024-04-13 00:31:55 +0200

    fix share button canShare

--- components/base/button/Share.tsx
+++ components/base/button/Share.tsx
@@ -9,11 +9,17 @@ interface IProps {
 
 type ButtonWithIconRefType = HTMLButtonElement
 
+interface IShareData {
+    url: string
+    title: string
+    description: string
+}
+
 const ShareButton = forwardRef<ButtonWithIconRefType, IProps>((props, buttonRef) => {
 
     const { clickCallback, ...rest } = props
 
-    const [canShareState, setCanShareState] = useState(false)
+    const [shareDataState, setShareDataState] = useState<IShareData | null>(null)
 
     const buttonClickHandler = async (/*event: React.MouseEvent<HTMLButtonElement>*/) => {
 
@@ -21,39 +27,46 @@ const ShareButton = forwardRef<ButtonWithIconRefType, IProps>((props, buttonRef)
             clickCallback()
         }
 
-        const ogUrl = document.querySelector('meta[property=og:url]') as HTMLMetaElement
-        const url = ogUrl ? ogUrl.content : document.location.href
-
-        const ogTitle = document.querySelector('meta[property=og:title]') as HTMLMetaElement
-        const title = ogTitle ? ogTitle.content : 'Chris.lu'
-
-        const ogDescription = document.querySelector('meta[property=og:description]') as HTMLMetaElement
-        const defaultDescription = 'chrisweb\'s blog about web development, games, Lego, music, memes, ... | chris.lu'
-        const description = ogDescription ? ogDescription.content : defaultDescription
-
-        const shareData = { url, title, description }
-
-        try {
-            await window.navigator
-                .share(shareData)
-                .then(() => {
-                    console.log('shared')
-                })
-        } catch (error) {
-            console.log(`error: ${error}`)
+        if (shareDataState !== null) {
+            try {
+                await window.navigator
+                    .share(shareDataState)
+                    .then(() => {
+                        if (process.env.NODE_ENV === 'development') {
+                            console.log('shared')
+                        }
+                    })
+            } catch (error) {
+                if (process.env.NODE_ENV === 'development') {
+                    console.log(`error: ${error}`)
+                }
+            }
         }
 
     }
 
     useEffect(() => {
-        if (window.navigator.canShare()) {
-            setCanShareState(true)
+
+        const ogUrl = document.querySelector('meta[property=\'og:url\']') as HTMLMetaElement
+        const url = ogUrl ? ogUrl.content : document.location.href
+
+        const ogTitle = document.querySelector('meta[property=\'og:title\']') as HTMLMetaElement
+        const title = ogTitle ? ogTitle.content : 'Chris.lu'
+
+        const ogDescription = document.querySelector('meta[property=\'og:description\']') as HTMLMetaElement
+        const defaultDescription = 'chrisweb\'s blog about web development, games, Lego, music, memes, ... | chris.lu'
+        const description = ogDescription ? ogDescription.content : defaultDescription
+
+        const shareData = { url, title, description }
+
+        if (window.navigator.canShare(shareData)) {
+            setShareDataState(shareData)
         }
     }, [])
 
     return (
         <>
-            {canShareState &&
+            {(shareDataState !== null) &&
                 <button
                     onClick={buttonClickHandler}
                     className={`${styles.reset} ${styles.base} ${styles.cursor}`}
