2946b90a8b5f9cdc007b3613cc55df18cb736adb
chrisweb <chris963@gmail.com>
2024-04-18 02:38:59 +0200

    re-add configuration for react and react hooks now that we don't use eslint-config-next anymore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
     root: true,
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
     'env': {
-        browser: true,
         es2021: true,
         node: true,
+        browser: true,
     },
     ignorePatterns: [
         'node_modules/',
@@ -16,17 +12,16 @@ module.exports = {
         'public/',
     ],
     reportUnusedDisableDirectives: true,
-    // ignore all by default
-    // we re-eanble each by using overrides
-    //ignorePatterns: ['**/*.ts?(x)', '**/*.md?(x)'],
     overrides: [
         {
             files: ['**/*.ts?(x)'],
             extends: [
                 'eslint:recommended',
                 'plugin:@typescript-eslint/recommended',
+                'plugin:react/recommended',
+                'plugin:react-hooks/recommended',
                 'plugin:@react-three/recommended',
-                'plugin:@next/next/recommended',
+                'plugin:@next/next/core-web-vitals',
             ],
             parser: '@typescript-eslint/parser',
             parserOptions: {
@@ -37,18 +32,34 @@ module.exports = {
                 warnOnUnsupportedTypeScriptVersion: true,
             },
             plugins: [
+                'import',
                 'react',
+                'jsx-a11y',
                 '@typescript-eslint',
                 '@react-three',
                 '@next/next',
             ],
             rules: {
-                // Indentation BUG: https://github.com/eslint/eslint/issues/16385
-                /*
-                indent: [
-                    'error',
-                    4,
-                ],*/
+                // rules copied from the eslint-config-next package
+                // https://github.com/vercel/next.js/blob/canary/packages/eslint-config-next/index.js
+                'import/no-anonymous-default-export': 'warn',
+                'react/no-unknown-property': 'off',
+                'react/react-in-jsx-scope': 'off',
+                'react/prop-types': 'off',
+                'jsx-a11y/alt-text': [
+                    'warn',
+                    {
+                        elements: ['img'],
+                        img: ['Image'],
+                    },
+                ],
+                'jsx-a11y/aria-props': 'warn',
+                'jsx-a11y/aria-proptypes': 'warn',
+                'jsx-a11y/aria-unsupported-elements': 'warn',
+                'jsx-a11y/role-has-required-aria-props': 'warn',
+                'jsx-a11y/role-supports-aria-props': 'warn',
+                'react/jsx-no-target-blank': 'off',
+                // our custom rules
                 quotes: [
                     'error',
                     'single',
@@ -81,6 +92,28 @@ module.exports = {
                         minimumDescriptionLength: 3,
                     },
                 ],
+            },
+            settings: {
+                react: {
+                    version: 'detect',
+                },
+                'import/parsers': {
+                    [require.resolve('@typescript-eslint/parser')]: [
+                        '.ts',
+                        '.mts',
+                        '.cts',
+                        '.tsx',
+                        '.d.ts',
+                    ],
+                },
+                'import/resolver': {
+                    [require.resolve('eslint-import-resolver-node')]: {
+                        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                    },
+                    [require.resolve('eslint-import-resolver-typescript')]: {
+                        alwaysTryTypes: true,
+                    },
+                },
             },
         },
         {
