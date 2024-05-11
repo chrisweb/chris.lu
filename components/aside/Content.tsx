887f28f1559be09f0b906e5af575b54ef7d4d9b3
chrisweb <chris963@gmail.com>
2024-05-11 18:55:15 +0200

    add icons to make the bug report info text and discussions text more visible

--- components/aside/Content.tsx
+++ components/aside/Content.tsx
@@ -2,6 +2,8 @@ import styles from './content.module.css'
 import Image from 'next/image'
 import BaseLink from '@/components/base/Link'
 import ShareButton from '@/components/base/button/Share'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBug, faComments } from '@fortawesome/free-solid-svg-icons'
 
 const AsideContent: React.FC = (): JSX.Element => {
 
@@ -14,7 +16,8 @@ const AsideContent: React.FC = (): JSX.Element => {
             <br />
             <span className="fontDarker">* Please 😉</span>
             <p className="fontSmall fontDarker alignLeft">Donations are not mandatory but greatly appreciated, as they allow me to work on more content (like the one on this page)</p>
-            <p className="fontSmall fontDarker alignLeft">If you find a bug / typo or you want to suggest a new feature, then please open an <BaseLink href="https://github.com/chrisweb/chris.lu/issues">Issue</BaseLink> on GitHub. Suggestions and Ideas are appricated, please use the <BaseLink href="https://github.com/chrisweb/chris.lu/discussions">discussion board</BaseLink> to leave feedback or ask a question.</p>
+            <p className="fontSmall fontDarker alignLeft"><FontAwesomeIcon icon={faBug} size="1x" color='rgb(255, 0, 170)' /> If you find a bug / typo or you want to suggest a new feature, then please open an <BaseLink href="https://github.com/chrisweb/chris.lu/issues">Issue</BaseLink> on GitHub.</p>
+            <p className="fontSmall fontDarker alignLeft"><FontAwesomeIcon icon={faComments} size="1x" color='rgb(255, 0, 170)' /> Suggestions and Ideas are appricated, please use the <BaseLink href="https://github.com/chrisweb/chris.lu/discussions">discussion board</BaseLink> to leave feedback or ask a question.</p>
         </div>
     )
 }
