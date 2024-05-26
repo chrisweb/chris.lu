d942b55af988227edf83986485463dd10f526894
chrisweb <chris963@gmail.com>
2024-05-26 23:39:35 +0200

    dynamic placeholder generator for gifs

--- components/base/image/Dispatch.tsx
+++ components/base/image/Dispatch.tsx
@@ -1,21 +1,35 @@
 import Image from 'next/image'
-import type { ImageProps } from 'next/image'
+import type { ImageProps, StaticImageData } from 'next/image'
 import ImageWithDialog from '@/components/base/image/WithDialog'
 
 const ImageDispatch: React.FC<ImageProps> = (props): JSX.Element => {
 
-    let optimizable = true
+    const staticImageData = props.src as StaticImageData
+    const src = staticImageData.src
 
-    const imageProps = props.src as ImageProps
-    const src = imageProps.src as string
+    const imageProps = {...props}
+
+    // Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
+    const keyStr =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+    const triplet = (e1: number, e2: number, e3: number) =>
+        keyStr.charAt(e1 >> 2) +
+        keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+        keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+        keyStr.charAt(e3 & 63)
+
+    const rgbDataURL = (r: number, g: number, b: number) =>
+        `data:image/gif;base64,R0lGODlhAQABAPAA${triplet(0, r, g) + triplet(b, 255, 255)
+        }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
 
     if (src.endsWith('gif')) {
-        optimizable = false
+        imageProps.blurDataURL = rgbDataURL(255,0,125)
     }
 
     return (
         <>
-            {props.alt.startsWith('banner') ? (
+            {imageProps.alt.startsWith('banner') ? (
                 <>
                     {/* eslint-disable-next-line jsx-a11y/alt-text */}
                     <Image
@@ -25,12 +39,12 @@ const ImageDispatch: React.FC<ImageProps> = (props): JSX.Element => {
                         }}
                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         priority
-                        placeholder={optimizable ? 'blur' : 'empty'}
-                        {...(props)}
+                        placeholder={'blur'}
+                        {...(imageProps)}
                     />
                 </>
-            ) : (props.alt.startsWith('meme') || props.alt.startsWith('photo') || props.alt.startsWith('screenshot')) ? (
-                <ImageWithDialog {...(props)} />
+            ) : (imageProps.alt.startsWith('meme') || imageProps.alt.startsWith('photo') || imageProps.alt.startsWith('screenshot')) ? (
+                <ImageWithDialog {...(imageProps)} />
             ) : (
                 <>
                     {/* eslint-disable-next-line jsx-a11y/alt-text */}
@@ -39,9 +53,8 @@ const ImageDispatch: React.FC<ImageProps> = (props): JSX.Element => {
                             maxWidth: '100%',
                             height: 'auto',
                         }}
-                        placeholder={optimizable ? 'blur' : 'empty'}
-                        unoptimized={optimizable ? false : true}
-                        {...(props)}
+                        placeholder={'blur'}
+                        {...(imageProps)}
                     />
                 </>
             )}
