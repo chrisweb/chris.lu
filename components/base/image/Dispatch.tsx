7e78d82a1df247455326b02c7df0ae103550f2df
chrisweb <chris963@gmail.com>
2024-06-15 11:07:02 +0200

    update to new dispatch technique

--- components/base/image/Dispatch.tsx
+++ components/base/image/Dispatch.tsx
@@ -2,34 +2,65 @@ import Image from 'next/image'
 import type { ImageProps, StaticImageData } from 'next/image'
 import ImageWithDialog from '@/components/base/image/WithDialog'
 
+// Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
+const keyStr =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+const triplet = (e1: number, e2: number, e3: number) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63)
+
+function rgbDataURL(r: number, g: number, b: number) {
+    return `data:image/gif;base64,R0lGODlhAQABAPAA${triplet(0, r, g) + triplet(b, 255, 255)}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+}
+
+function extractImageType(imageProps: ImageProps) {
+
+    let imageType = ''
+
+    if (imageProps !== null) {
+
+        const imageTypeMatch = imageProps.title?.match(/\{(.*?)\}/)
+
+        if (imageTypeMatch) {
+            // the full match, for example "{ banner }"
+            const imageTypeFull = imageTypeMatch[0]
+            // the type only, for example "banner"
+            imageType = imageTypeMatch[1].trim()
+            // remove the image type info from the title
+            const newTitle = imageProps.title?.replace(imageTypeFull, '').trim()
+
+            if (newTitle !== '') {
+                imageProps.title = newTitle
+            } else {
+                delete imageProps.title
+            }
+        }
+
+    }
+
+    return imageType
+
+}
+
 const ImageDispatch: React.FC<ImageProps> = (props): JSX.Element => {
+
+    const newImageProps = { ...props }
+
+    const imageType = extractImageType(newImageProps)
 
     const staticImageData = props.src as StaticImageData
     const src = staticImageData.src
 
-    const imageProps = { ...props }
-
-    // Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
-    const keyStr =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
-
-    const triplet = (e1: number, e2: number, e3: number) =>
-        keyStr.charAt(e1 >> 2) +
-        keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-        keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-        keyStr.charAt(e3 & 63)
-
-    const rgbDataURL = (r: number, g: number, b: number) =>
-        `data:image/gif;base64,R0lGODlhAQABAPAA${triplet(0, r, g) + triplet(b, 255, 255)
-        }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
-
     if (src?.endsWith('gif')) {
-        imageProps.blurDataURL = rgbDataURL(255, 0, 125)
+        newImageProps.blurDataURL = rgbDataURL(255, 0, 125)
     }
 
     return (
         <>
-            {imageProps.alt.startsWith('banner') ? (
+            {imageType === 'banner' ? (
                 <>
                     {/* eslint-disable-next-line jsx-a11y/alt-text */}
                     <Image
@@ -40,12 +71,12 @@ const ImageDispatch: React.FC<ImageProps> = (props): JSX.Element => {
                         sizes="(max-width: 48rem) 100vw, 704px"
                         priority
                         placeholder="blur"
-                        {...(imageProps)}
+                        {...(newImageProps)}
                     />
                 </>
-            ) : (imageProps.alt.startsWith('meme') || imageProps.alt.startsWith('photo') || imageProps.alt.startsWith('screenshot')) ? (
-                <ImageWithDialog {...(imageProps)} />
-            ) : (imageProps.alt.startsWith('card')) ? (
+            ) : (imageType === 'meme' || imageType === 'photo' || imageType === 'screenshot') ? (
+                <ImageWithDialog {...(newImageProps)} />
+            ) : (imageType === 'card') ? (
                 <>
                     {/* eslint-disable-next-line jsx-a11y/alt-text */}
                     <Image
@@ -55,7 +86,7 @@ const ImageDispatch: React.FC<ImageProps> = (props): JSX.Element => {
                         fill
                         sizes="(max-width: 48rem) 100vw, 336px"
                         placeholder="blur"
-                        {...props}
+                        {...newImageProps}
                     />
                 </>
             ) : (
@@ -68,7 +99,7 @@ const ImageDispatch: React.FC<ImageProps> = (props): JSX.Element => {
                         }}
                         sizes="(max-width: 48rem) 100vw, 704px"
                         placeholder="blur"
-                        {...(imageProps)}
+                        {...(newImageProps)}
                     />
                 </>
             )}
