97619fb5743b2b294192c31c79524a2dc6b3854a
chrisweb <chris963@gmail.com>
2024-06-16 23:52:37 +0200

    video play on scroll experiment (wip)

--- components/video/Scroll.tsx
+++ components/video/Scroll.tsx
@@ -5,46 +5,65 @@ import { useRef, useEffect, useCallback } from 'react'
 const VideoScroll: React.FC = () => {
 
     const videoRef = useRef<HTMLVideoElement>(null)
+    const requestAnimationFrameRef = useRef(0)
 
-    //const frameNumber = 0 // start video at frame 0
-    // lower numbers = faster playback
-    const playbackConst = 100
-
-    // Use requestAnimationFrame for smooth playback
     const scrollPlay = useCallback(() => {
-        const frameNumber = window.scrollY / playbackConst
 
-console.log(window.scrollY)
-console.log(frameNumber)
+        let frame = 0
+        let chunkLength = 0
 
-        if (videoRef.current !== null) {
-            videoRef.current.currentTime = frameNumber
+        if (window.scrollY > window.innerHeight) {
+            requestAnimationFrameRef.current = window.requestAnimationFrame(scrollPlay)
+            return
         }
-        window.requestAnimationFrame(scrollPlay)
+
+        if (typeof videoRef.current?.duration === 'number') {
+            chunkLength = videoRef.current.duration / window.innerHeight
+        }
+
+        frame = window.scrollY * chunkLength
+/*
+console.log(videoRef.current?.duration)
+console.log(chunkLength)
+console.log(window.scrollY)
+console.log(window.innerHeight)
+console.log(frame)
+//console.log(videoRef.current)
+console.log('------')
+//cancelAnimationFrame(requestAnimationFrameRef.current)
+*/
+
+        if (videoRef.current && chunkLength > 0) {
+            videoRef.current.currentTime = frame
+            requestAnimationFrameRef.current = window.requestAnimationFrame(scrollPlay)
+        }
+        
     }, [])
+
+    const start = useCallback(() => {
+console.log('#### starting')
+        requestAnimationFrameRef.current = window.requestAnimationFrame(scrollPlay)
+    }, [scrollPlay])
 
     useEffect(() => {
 
-        // get page height from video duration
-        //const setHeight = window.document.getElementById('set-height')
+        const video = videoRef.current
 
-        // dynamically set the page height according to video length
-        videoRef.current?.addEventListener('loadedmetadata', function () {
-            /*if (setHeight !== null && typeof videoRef.current?.duration === 'number') {
-                setHeight.style.height = Math.floor(videoRef.current.duration) * playbackConst + 'px'
-            }*/
+        video?.addEventListener('loadedmetadata', start)
 
-                
-        })
+        video?.load()
 
-        window.requestAnimationFrame(scrollPlay)
+        return () => {
+            cancelAnimationFrame(requestAnimationFrameRef.current)
+            video?.removeEventListener('loadedmetadata', start)
+        }
 
-    }, [scrollPlay])
+    }, [start])
 
     return (
         <>
             <div id="set-height"></div>
-            <video ref={videoRef} controls preload="preload" title="a voodoo lady mixing potions in a big cauldron, it represents a dev using different packages to build a project using an IDE" poster="/assets/images/app/web_development/tutorials/next-js-static-mdx-blog/banner.png">
+            <video ref={videoRef} muted playsInline preload="auto" title="a voodoo lady mixing potions in a big cauldron, it represents a dev using different packages to build a project using an IDE" poster="/assets/images/app/web_development/tutorials/next-js-static-mdx-blog/banner.png">
                 <source src="/assets/video/app/web_development/tutorials/next-js-static-mdx-blog/banner.mp4" type="video/mp4" />
                 <p>Your browser doesn&apos;t support HTML5 video.</p>
             </video>
