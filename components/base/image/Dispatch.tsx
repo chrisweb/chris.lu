7b9c1a03b17960ce2d0be100fd002845024458ac
chrisweb <chris963@gmail.com>
2024-07-05 22:20:57 +0200

    move blur data url generator to lib

--- components/base/image/Dispatch.tsx
+++ components/base/image/Dispatch.tsx
@@ -1,20 +1,7 @@
 import Image from 'next/image'
 import type { ImageProps, StaticImageData } from 'next/image'
 import ImageWithDialog from '@/components/base/image/WithDialog'
-
-// Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
-const keyStr =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
-
-const triplet = (e1: number, e2: number, e3: number) =>
-    keyStr.charAt(e1 >> 2) +
-    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-    keyStr.charAt(e3 & 63)
-
-function rgbDataURL(r: number, g: number, b: number) {
-    return `data:image/gif;base64,R0lGODlhAQABAPAA${triplet(0, r, g) + triplet(b, 255, 255)}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
-}
+import { rgbDataURL } from '@/lib/image'
 
 function extractImageType(imageProps: ImageProps) {
 
