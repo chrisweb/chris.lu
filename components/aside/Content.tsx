41f8fe8e74ddd0b567760727e1ab18272440a533
chrisweb <chris963@gmail.com>
2024-08-03 16:47:34 +0200

    add donation message to more posts

--- components/aside/Content.tsx
+++ components/aside/Content.tsx
@@ -2,7 +2,7 @@ import styles from './content.module.css'
 import BaseLink from '@/components/base/Link'
 import ShareButton from '@/components/base/button/Share'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBug, faComments } from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faBug, faComments } from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
 import buyMeACoffeeImport from '/public/assets/images/buy_me_a_coffee_button.png'
 
@@ -16,7 +16,7 @@ const AsideContent: React.FC = (): JSX.Element => {
             </a>
             <br />
             <span className="fontDarker">* Please 😉</span>
-            <p className="fontSmall fontDarker alignLeft">Donations are not mandatory but greatly appreciated, as they allow me to work on more content (like the one on this page)</p>
+            <p className="fontSmall fontDarker alignLeft"><FontAwesomeIcon icon={faHeart} size="1x" color='rgb(255, 0, 170)' /><BaseLink href="https://buymeacoffee.com/chriswwweb">Donations</BaseLink> are not mandatory but greatly appreciated, as they allow me to work on more content and keep the project free for everyone</p>
             <p className="fontSmall fontDarker alignLeft"><FontAwesomeIcon icon={faBug} size="1x" color='rgb(255, 0, 170)' /> If you find a bug / typo or you want to suggest a new feature, then please open an <BaseLink href="https://github.com/chrisweb/chris.lu/issues">Issue</BaseLink> on GitHub.</p>
             <p className="fontSmall fontDarker alignLeft"><FontAwesomeIcon icon={faComments} size="1x" color='rgb(255, 0, 170)' /> Suggestions and Ideas are appricated, please use the <BaseLink href="https://github.com/chrisweb/chris.lu/discussions">discussion board</BaseLink> to leave feedback or ask a question.</p>
         </div>
