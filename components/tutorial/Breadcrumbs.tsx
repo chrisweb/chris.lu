4b1c6d346eeee0663dc9cc0e3e0e2147e3f92d35
chrisweb <chris963@gmail.com>
2024-08-10 23:08:18 +0200

    new breadcrumbs and pagination components

---
+++ components/tutorial/Breadcrumbs.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import type { Route } from 'next'
+import styles from './breadcrumbs.module.css'
+
+export interface IListItem {
+    label: string
+    href: Route<string> | URL
+}
+
+export interface IPaginationProps {
+    list: IListItem[]
+}
+
+const Breadcrumbs: React.FC<IPaginationProps> = (props): JSX.Element => {
+
+    const { list } = props
+
+    return (
+        <>
+            <nav className={styles.nav} aria-label="Breadcrumbs">
+                    {list.map(listItem =>
+                        <div key={crypto.randomUUID()} className={`fontSmall ${styles.inline}`}>
+                            <Link href={listItem.href}>{listItem.label}</Link>
+                            <FontAwesomeIcon icon={faChevronRight} size="sm" className={styles.icon} />
+                        </div>
+                    )}
+            </nav>
+        </>
+    )
+}
+
+export default Breadcrumbs
