ce7b5b970123272d4350c83c5b70d569c5b1aa79
chrisweb <chris963@gmail.com>
2024-11-13 19:24:12 +0100

    use record (preferred by linter), fix an image entry for sentry post

--- shared/image-info.ts
+++ shared/image-info.ts
@@ -1,8 +1,6 @@
-interface IImageInfo {
-    [key: string]: string[]
-}
+type ImageInfoType = Record<string, string[]>
 
-export const imageInfo: IImageInfo = {
+export const imageInfo: ImageInfoType = {
     'posts_csp': ['Content Security Policy (CSP)', 'posts/csp'],
     'posts_git': ['git', 'posts/git'],
     'posts_github': ['GitHub', 'posts/github'],
@@ -10,7 +8,7 @@ export const imageInfo: IImageInfo = {
     'posts_node-js': ['Node.js', 'posts/node-js'],
     'posts_npm': ['npm & package.json', 'posts/npm'],
     'posts_road-to-react-19-next-js-15': ['The road to React 19 and Next.js 15', 'posts/road-to-react-19-next-js-15'],
-    'posts_senty-io': ['Sentry.io', 'posts/senty-io'],
+    'posts_sentry-io': ['Sentry.io', 'posts/sentry-io'],
     'posts_vercel': ['Vercel', 'posts/vercel'],
     'posts_vscode': ['VSCode', 'posts/vscode'],
     'tutorials_next-js-static-first-mdx-starterkit': ['Tutorial: Next.js (15) static first MDX starterkit', 'tutorials/next-js-static-first-mdx-starterkit'],
