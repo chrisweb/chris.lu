f5abcfd09db05a4147c910cde9c6de3a1f69d53c
chrisweb <chris963@gmail.com>
2024-11-27 15:52:44 +0100

    fix typo in class name

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,7 +95,7 @@ export default function RootLayout({ children }: {
                     {children}
                 </main>
                 <footer className={styles.layoutFooter}>
-                    <hr className={styles.layoutFooterSeperator} />
+                    <hr className={styles.layoutFooterSeparator} />
                     <p className="fontDarker">All content on this site is licensed under a <BaseLink href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</BaseLink> license. The source code of this project is licensed under <BaseLink href="https://github.com/chrisweb/chris.lu/blob/main/LICENSE">MIT</BaseLink> and a copy of the source code can be found in the <BaseLink href="https://github.com/chrisweb/chris.lu">chris.lu public GitHub respository</BaseLink>. A list of all open source packages used to build this project can be found in the <BaseLink href="https://github.com/chrisweb/chris.lu/blob/main/package.json">package.json</BaseLink> file. This website uses music licensed under different creative commons licenses, the music tracks <BaseLink href="https://github.com/chrisweb/chris.lu/blob/main/public/assets/music/CREDITS.txt">credits</BaseLink> file can be found in the repository of this project or by clicking on the &quot;eject&quot; button of the player on the top right of the screen. This website uses <BaseLink href="https://fontawesome.com/search?o=r&m=free">Free Icons by Font Awesome</BaseLink>.</p>
                 </footer>
                 <Analytics />
