95068f7b176ee50c4b48596c11a948e2e5c6d91d
chrisweb <chris963@gmail.com>
2024-12-02 20:19:12 +0100

    simplify camera code

--- components/neonRoad/Canvas.tsx
+++ components/neonRoad/Canvas.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { useRef, Suspense } from 'react'
-import type { PerspectiveCamera as PerspectiveCameraType } from 'three'
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera, SoftShadows, AdaptiveDpr/*, OrbitControls*//*, PerformanceMonitor, PerformanceMonitorApi/*, Hud, useDetectGPU, useProgress, StatsGl*/ } from '@react-three/drei'
 import NightSky from './NightSky'
@@ -10,7 +9,6 @@ import SunLight from './SunLight'
 import City from './City'
 import Trees from './Trees'
 import Terrains from './Terrains'
-//import Loop from './Loop'
 //import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
 interface IProps extends React.PropsWithChildren {
@@ -20,16 +18,11 @@ interface IProps extends React.PropsWithChildren {
 
 const NeonRoadCanvas: React.FC<IProps> = (props) => {
 
-    //const [canPlayState, setCanPlayState] = useState(false)
-
     // uncomment if you want to see what useDetectGPU returns
     //const gpuInfo = useDetectGPU()
     //if (process.env.NODE_ENV === 'development') {
     //console.log('useDetectGPU: ', gpuInfo)
     //}
-
-    //const canvasRef = useRef<HTMLCanvasElement>(null)
-    const cameraRef = useRef<PerspectiveCameraType>(null)
 
     // https://docs.pmnd.rs/react-three-fiber/tutorials/v8-migration-guide#expanded-gl-prop
     // https://threejs.org/docs/#api/en/renderers/WebGLRenderer
@@ -90,15 +83,12 @@ const NeonRoadCanvas: React.FC<IProps> = (props) => {
                 gl={rendererProps}
                 //frameloop="never"
                 //onCreated={onCanvasCreatedHandler}
-                //ref={canvasRef}
-                camera={cameraRef.current ?? {}}
             >
                 <Suspense fallback={<Fallback />}>
                     <AdaptiveDpr pixelated />
                     {/*<Loader />*/}
                     <PerspectiveCamera
                         makeDefault={true}
-                        ref={cameraRef}
                         fov={75}
                         near={0.01}
                         far={3}
@@ -127,7 +117,6 @@ const NeonRoadCanvas: React.FC<IProps> = (props) => {
                     />
                     <Trees />
                     <Terrains />
-                    {/*<Loop />*/}
                     {/* <EffectComposer>
                         <Bloom
                             luminanceThreshold={0.08}
