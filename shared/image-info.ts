bda3e915ec62e45f21cc6e19aa689dfa739bd0f3
chrisweb <chris963@gmail.com>
2024-12-07 13:44:35 +0100

    adjust the title of new tutorial for og image alt

--- shared/image-info.ts
+++ shared/image-info.ts
@@ -1,7 +1,7 @@
 type ImageInfoType = Record<string, string[]>
 
 export const imageInfo: ImageInfoType = {
-    'posts_csp': ['Content Security Policy (CSP)', 'posts/csp'],
+    'posts_csp': ['Content Security Policy (CSP) header', 'posts/csp'],
     'posts_git': ['git', 'posts/git'],
     'posts_github': ['GitHub', 'posts/github'],
     'posts_mdx': ['MDX', 'posts/mdx'],
@@ -11,6 +11,6 @@ export const imageInfo: ImageInfoType = {
     'posts_sentry-io': ['Sentry.io', 'posts/sentry-io'],
     'posts_vercel': ['Vercel', 'posts/vercel'],
     'posts_vscode': ['VSCode', 'posts/vscode'],
-    'tutorials_next-js-static-first-mdx-starterkit': ['Tutorial: Next.js (15) static first MDX starterkit', 'tutorials/next-js-static-first-mdx-starterkit'],
-    'tutorials_next-static-mdx-blog': ['Tutorial: Next.js (14) static MDX blog', 'tutorials/next-js-static-mdx-blog'],
+    'tutorials_next-static-mdx-blog': ['Tutorial: Next.js 14 static MDX blog', 'tutorials/next-js-static-mdx-blog'],
+    'tutorials_next-js-static-first-mdx-starterkit': ['Tutorial: Next.js 15 static first MDX starterkit', 'tutorials/next-js-static-first-mdx-starterkit'],
 }
