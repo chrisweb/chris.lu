84a0b2f43176ec9a414c49c06211291f0a674644
chrisweb <chris963@gmail.com>
2024-12-09 14:10:25 +0100

    improve mdx-components types

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,9 @@
+import type { ComponentPropsWithoutRef } from 'react'
 import type { MDXComponents } from 'mdx/types'
 import TocHighlight from '@/components/toc/Highlight'
 import BaseLink from '@/components/base/Link'
 import ImageDispatch from '@/components/base/image/Dispatch'
-import { ImageProps } from 'next/image'
+import type { ImageProps } from 'next/image'
 import type { Route } from 'next'
 import AsideContent from '@/components/aside/Content'
 
@@ -11,47 +12,52 @@ import AsideContent from '@/components/aside/Content'
 // React component you want, including components from
 // other libraries.
 
+type HeadingPropsType = ComponentPropsWithoutRef<'h1'>
+type AnchorPropsType = ComponentPropsWithoutRef<'a'>
+// ImageProps get imported from 'next/image'
+type AsidePropsType = ComponentPropsWithoutRef<'aside'>
+
 // This file is required to use MDX in `app` directory.
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         // Allows customizing built-in components, e.g. to add styling.
-        h1: ({ children, ...props }) => (
+        h1: ({ children, ...props }: HeadingPropsType) => (
             <h1 className="h1" {...props}>
                 {children}
             </h1>
         ),
-        h2: ({ children, ...props }) => (
+        h2: ({ children, ...props }: HeadingPropsType) => (
             <h2 className="h2" {...props}>
                 {children}
             </h2>
         ),
-        h3: ({ children, ...props }) => (
+        h3: ({ children, ...props }: HeadingPropsType) => (
             <h3 className="h3" {...props}>
                 {children}
             </h3>
         ),
-        h4: ({ children, ...props }) => (
+        h4: ({ children, ...props }: HeadingPropsType) => (
             <h4 className="h4" {...props}>
                 {children}
             </h4>
         ),
-        h5: ({ children, ...props }) => (
+        h5: ({ children, ...props }: HeadingPropsType) => (
             <h5 className="h5" {...props}>
                 {children}
             </h5>
         ),
-        h6: ({ children, ...props }) => (
+        h6: ({ children, ...props }: HeadingPropsType) => (
             <h6 className="h6" {...props}>
                 {children}
             </h6>
         ),
-        a: ({ children, href, ...props }) => (
+        a: ({ children, href, ...props }: AnchorPropsType) => (
             <BaseLink href={href as Route} {...props}>
                 {children}
             </BaseLink>
         ),
         img: props => (<ImageDispatch {...props as ImageProps} />),
-        aside: ({ children, ...props }) => (
+        aside: ({ children, ...props }: AsidePropsType) => (
             <>
                 {(props.id === 'articleToc') ?
                     (
