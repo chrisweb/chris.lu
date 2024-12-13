33c17ddb46fe10bffeaeffeb29be9a07270ca9fe
chrisweb <chris963@gmail.com>
2024-12-13 17:10:10 +0100

    remove unused tracesSampleRate

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,9 +8,6 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
     dsn: 'https://daf0befe66519725bbe2ad707a11bbb3@o4504017992482816.ingest.us.sentry.io/4506763918770176',
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
-
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
 })
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,13 +2,10 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs'
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
     dsn: 'https://daf0befe66519725bbe2ad707a11bbb3@o4504017992482816.ingest.us.sentry.io/4506763918770176',
-
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
