a783473cddbaf803646441fa7e9caf6e9a0a9510
chrisweb <chris963@gmail.com>
2024-12-14 12:29:51 +0100

    formatting

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,7 +2,7 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
     dsn: 'https://daf0befe66519725bbe2ad707a11bbb3@o4504017992482816.ingest.us.sentry.io/4506763918770176',
