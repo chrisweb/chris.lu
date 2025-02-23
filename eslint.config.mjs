1ca65aef0dfd871fab678437a0b335c2bd781719
chrisweb <chris963@gmail.com>
2025-02-23 16:48:16 +0100

    customize eslint @stylistic/js/multiline-ternary linting rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -123,7 +123,7 @@ const nextConfig = [
             'jsx-a11y/aria-unsupported-elements': 'warn',
             'jsx-a11y/role-has-required-aria-props': 'warn',
             'jsx-a11y/role-supports-aria-props': 'warn',
-
+            '@stylistic/js/multiline-ternary': ['warn', { ignoreJSX: true }],
         },
         settings: {
             'react': {
