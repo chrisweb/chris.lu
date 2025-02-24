39272f7d88349e5bd660d9154876eb2e393a8938
chrisweb <chris963@gmail.com>
2025-02-24 09:37:31 +0100

    I usually dislike multiple returns, but in this case it is much cleaner

--- components/base/image/Dispatch.tsx
+++ components/base/image/Dispatch.tsx
@@ -4,9 +4,7 @@ import ImageWithDialog, { type IImageWithDialog } from '@/components/base/image/
 import { rgbDataURL } from '@/lib/image'
 
 function extractImageType(imageProps: ImageProps) {
-
     let imageType = ''
-
     const imageTypeMatch = imageProps.title?.match(/\{(.*?)\}/)
 
     if (imageTypeMatch) {
@@ -22,19 +20,15 @@ function extractImageType(imageProps: ImageProps) {
         } else {
             delete imageProps.title
         }
-
     }
 
     return imageType
-
 }
 
 const ImageDispatch: React.FC<ImageProps> = (props) => {
-
-    const newImageProps = { ...props }
-
-    const imageType = extractImageType(newImageProps)
-
+    const { alt, ...rest } = props
+    const newImageProps = { ...rest }
+    const imageType = extractImageType(props)
     const staticImageData = props.src as StaticImageData
     const src = staticImageData.src
 
@@ -42,74 +36,76 @@ const ImageDispatch: React.FC<ImageProps> = (props) => {
         newImageProps.blurDataURL = rgbDataURL(255, 0, 125)
     }
 
+    if (imageType === 'banner') {
+        return (
+            <Image
+                style={{
+                    width: '100%',
+                    height: 'auto'
+                }}
+                // 704px = 768px - (2 x 32px)
+                // banner width = middle max width - (2 x spacing)
+                sizes="(max-width: 768px) 100vw, 704px"
+                priority
+                placeholder="blur"
+                alt={alt || ''}
+                {...newImageProps}
+            />
+        )
+    }
+
+    if (imageType === 'meme' || imageType === 'photo' || imageType === 'screenshot') {
+        return (
+            <ImageWithDialog {...(newImageProps as IImageWithDialog)} alt={alt || ''} />
+        )
+    }
+
+    if (imageType === 'card') {
+        return (
+            <Image
+                style={{
+                    objectFit: 'cover'
+                }}
+                fill
+                // 336px = (768px - (2 x 32px) - 32px) / 2
+                // thumbnail width = (middle max width - (2 x spacing) - grid space between two columns) / 2
+                sizes="(max-width: 768px) 100vw, 336px"
+                placeholder="blur"
+                alt={alt || ''}
+                {...newImageProps}
+            />
+        )
+    }
+
+    if (imageType === 'animated') {
+        return (
+            <Image
+                style={{
+                    width: '480px',
+                    maxWidth: '100%',
+                    height: 'auto'
+                }}
+                unoptimized
+                placeholder="blur"
+                alt={alt || ''}
+                {...newImageProps}
+            />
+        )
+    }
+
     return (
-        <>
-            {(imageType === 'banner') ? (
-                <>
-                    {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                    <Image
-                        style={{
-                            width: '100%',
-                            height: 'auto',
-                        }}
-                        // 704px = 768px - (2 x 32px)
-                        // banner width = middle max width - (2 x spacing)
-                        sizes="(max-width: 768px) 100vw, 704px"
-                        priority
-                        placeholder="blur"
-                        {...(newImageProps)}
-                    />
-                </>
-            ) : (imageType === 'meme' || imageType === 'photo' || imageType === 'screenshot') ? (
-                <>
-                    <ImageWithDialog {...(newImageProps as IImageWithDialog)} alt={newImageProps.alt || ''} />
-                </>
-            ) : (imageType === 'card') ? (
-                <>
-                    {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                    <Image
-                        style={{
-                            objectFit: 'cover',
-                        }}
-                        fill
-                        // 336px = (768px - (2 x 32px) - 32px) / 2
-                        // thumbnail width = (middle max width - (2 x spacing) - grid space between two columns) / 2
-                        sizes="(max-width: 768px) 100vw, 336px"
-                        placeholder="blur"
-                        {...newImageProps}
-                    />
-                </>
-            ) : (imageType === 'animated') ? (
-                <>
-                    {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                    <Image
-                        style={{
-                            width: '480px',
-                            maxWidth: '100%',
-                            height: 'auto',
-                        }}
-                        unoptimized
-                        placeholder="blur"
-                        {...(newImageProps)}
-                    />
-                </>
-            ) : (
-                <>
-                    {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                    <Image
-                        style={{
-                            maxWidth: '100%',
-                            height: 'auto',
-                        }}
-                        // 704px = 768px - (2 x 32px)
-                        // banner width = middle max width - (2 x spacing)
-                        sizes="(max-width: 768px) 100vw, 704px"
-                        placeholder="blur"
-                        {...(newImageProps)}
-                    />
-                </>
-            )}
-        </>
+        <Image
+            style={{
+                maxWidth: '100%',
+                height: 'auto'
+            }}
+            // 704px = 768px - (2 x 32px)
+            // banner width = middle max width - (2 x spacing)
+            sizes="(max-width: 768px) 100vw, 704px"
+            placeholder="blur"
+            alt={alt || ''}
+            {...newImageProps}
+        />
     )
 }
 
