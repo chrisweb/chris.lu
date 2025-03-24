0ce9466ebd1257a7b91219208b6a8c58974bfbef
chrisweb <chris963@gmail.com>
2025-03-24 11:01:40 +0100

    add bluesky social icon and link

--- components/aside/Content.tsx
+++ components/aside/Content.tsx
@@ -3,7 +3,7 @@ import BaseLink from '@/components/base/Link'
 import ShareButton from '@/components/base/button/Share'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faBug, faComments } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faMastodon, faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faMastodon, faDiscord, faBluesky } from '@fortawesome/free-brands-svg-icons'
 import Image from 'next/image'
 import buyMeACoffeeImport from '@/public/assets/images/buy_me_a_coffee_button.png'
 
@@ -41,11 +41,14 @@ const AsideContent: React.FC = () => {
                 <BaseLink href="https://discord.gg/4p59CCTUAJ" noExternalIcon={true}>
                     <FontAwesomeIcon icon={faDiscord} color="white" size="2x" className="socialIcon" />
                 </BaseLink>
-                <BaseLink href="https://mastodon.social/@chriswwweb" noExternalIcon={true}>
-                    <FontAwesomeIcon icon={faMastodon} color="white" size="2x" className="socialIcon" />
+                <BaseLink href="https://bsky.app/profile/chriswwweb.bsky.social" noExternalIcon={true}>
+                    <FontAwesomeIcon icon={faBluesky} color="white" size="2x" className="socialIcon" />
                 </BaseLink>
                 <BaseLink href="https://github.com/chrisweb" noExternalIcon={true}>
                     <FontAwesomeIcon icon={faGithub} color="white" size="2x" className="socialIcon" />
+                </BaseLink>
+                <BaseLink href="https://mastodon.social/@chriswwweb" noExternalIcon={true}>
+                    <FontAwesomeIcon icon={faMastodon} color="white" size="2x" className="socialIcon" />
                 </BaseLink>
             </p>
         </div>
