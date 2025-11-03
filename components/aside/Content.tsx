dccbb52492a24af04ec44e35cca6b1300f28794a
chrisweb <chris963@gmail.com>
2025-11-03 18:25:22 +0100

    small layout tweaks

--- components/aside/Content.tsx
+++ components/aside/Content.tsx
@@ -2,7 +2,6 @@ import styles from './content.module.css'
 import BaseLink from '@/components/base/Link'
 import ShareButton from '@/components/base/button/Share'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart, faBug, faComments } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faMastodon, faDiscord, faBluesky } from '@fortawesome/free-brands-svg-icons'
 import Image from 'next/image'
 import buyMeACoffeeImport from '@/public/assets/images/buy_me_a_coffee_button.png'
@@ -16,27 +15,6 @@ const AsideContent: React.FC = () => {
                 <Image src={buyMeACoffeeImport} priority fill alt="buy me a coffee, please" />
             </a>
             <span className="fontDarker">* Please 😉</span>
-            <p className="fontSmall fontDarker alignLeft">
-                <FontAwesomeIcon icon={faHeart} size="1x" className="startInlineIcon" />
-                <BaseLink href="https://buymeacoffee.com/chriswwweb">Buying me a coffee</BaseLink>
-                or&nbsp;
-                <BaseLink href="https://github.com/sponsors/chrisweb">sponsoring me</BaseLink>
-                on GitHub, are not mandatory but greatly appreciated, as it allows me to work on more content and keep the project free for everyone
-            </p>
-            <p className="fontSmall fontDarker alignLeft">
-                <FontAwesomeIcon icon={faBug} size="1x" className="startInlineIcon" />
-                If you find a bug / typo or you want to suggest a new feature, then please open an&nbsp;
-                <BaseLink href="https://github.com/chrisweb/chris.lu/issues">Issue</BaseLink>
-                on GitHub.
-            </p>
-            <p className="fontSmall fontDarker alignLeft">
-                <FontAwesomeIcon icon={faComments} className="startInlineIcon" />
-                Suggestions and Ideas are appreciated, please use the&nbsp;
-                <BaseLink href="https://discord.gg/4p59CCTUAJ">chris.lu Discord server</BaseLink>
-                or the&nbsp;
-                <BaseLink href="https://github.com/chrisweb/chris.lu/discussions">discussion board</BaseLink>
-                to leave feedback or ask a question.
-            </p>
             <p>
                 <BaseLink href="https://discord.gg/4p59CCTUAJ" noExternalIcon={true}>
                     <FontAwesomeIcon icon={faDiscord} color="white" size="2x" className="socialIcon" />
